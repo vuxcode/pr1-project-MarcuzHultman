dd00b740e4d61157555d4369ddd0e99f290a3412
Marcus Hultman <marcuzhultman@gmail.com>
2022-10-21 15:13:45 +0200

    1st problem

---
+++ script.js
@@ -0,0 +1,16 @@
+var card1 = document.querySelector(".card-1");
+var card2 = document.querySelector(".card-2");
+var card3 = document.querySelector(".card-3");
+var card4 = document.querySelector(".card-4");
+var card5 = document.querySelector(".card-5");
+var card6 = document.querySelector(".card-6");
+var card7 = document.querySelector(".card-7");
+var card8 = document.querySelector(".card-8");
+var card9 = document.querySelector(".card-9");
+var card10 = document.querySelector(".card-10");
+
+document.querySelector(".card-1").addEventListener("click", function () {
+  card1.classList.add("img-1");
+
+  console.log("click");
+});
